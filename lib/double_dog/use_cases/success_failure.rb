79b5c4e6c626519134243f1701d7f6bd77e68a67
Greg Cosgrove <gregcosgrove@gmail.com>
2014-06-02 13:10:49 -0500

    Refactor success failure method in sepearete class

---
+++ lib/double_dog/use_cases/success_failure.rb
@@ -0,0 +1,14 @@
+module DoubleDog
+  class SuccessFailure
+
+    private
+
+    def failure(error_name)
+      return :success? => false, :error => error_name
+    end
+
+    def success(data)
+      return data.merge(:success? => true)
+    end
+  end
+end
